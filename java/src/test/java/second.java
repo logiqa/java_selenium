2a7ec9d7406be13b57a14d7eaeb46bbef84af608
lololo <123123123>
2019-09-18 18:56:53 +0300

    new test chrom
    new test chrom

---
+++ java/src/test/java/second.java
@@ -0,0 +1,31 @@
+import jdk.jfr.StackTrace;
+import org.openqa.selenium.webdriver;
+import org.openqa.selenium.support.ui.Webdriverwait;
+
+
+public class second {
+
+    private Webdriver driver;
+    private WebdriverWait wait;
+
+    @Before
+    public  void start () {
+        driver = new chromedriver ();
+        wait = new Webdriver (driver, timeoutseconds 10 );
+    }
+    @Test
+    public void second (){
+        driver.get("https://bash.im/search");
+        driver.findeElement(By.name("text")).sendkeys ("1");
+        driver.findElement(By.tagname("button")).click();
+        wait.until(titleis("Цитата #1 - Цитатник Рунета"));
+    }
+    @After
+    public void stop() {
+        driver.quit();
+        driver = nill;
+
+    }    
+
+
+}
